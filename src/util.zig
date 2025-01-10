5daca5d234264a7e44c2cb99442893390b5c8c05
umer <bscs23211@itu.edu.pk>
2025-01-11 04:15:20 +0500

    chore: read file and trim whitespaces

--- src/util.zig
+++ src/util.zig
@@ -1,16 +1,67 @@
 const std = @import("std");
-const asm_extension: []const u8 = ".asm";
 
+// only .asm files allowed
 pub fn validFileExtension(file_name: []const u8) bool {
+    const asm_extension: []const u8 = ".asm";
+    // rets index of where .asm is found
     const validator = std.mem.indexOf(u8, file_name, asm_extension);
-    if (validator) |_| {
-        const file = std.fs.cwd().openFile(file_name, .{});
-        const file_res = file catch |err| {
-            std.debug.print("{}\n", .{err});
-            std.process.exit(1);
-        };
-        defer file_res.close();
+    // discard capture
+    if (validator) |_|
         return true;
-    }
     return false;
+}
+
+pub fn readFileStoreAndTrim(lines: *std.ArrayList([]const u8), allocator: *const std.mem.Allocator, file_name: []const u8) !void {
+    // buffer i.e: file line
+    var buf = std.ArrayList(u8).init(allocator.*);
+    defer buf.deinit();
+
+    // file
+    // err when file not found
+    const file = std.fs.cwd().openFile(file_name, .{}) catch |err|
+        switch (err) {
+        error.FileNotFound => {
+            std.debug.print("SASM: file not found.\n", .{});
+            std.process.exit(1);
+        },
+        else => {
+            std.debug.print("SASM: {}\n", .{err});
+            std.process.exit(1);
+        },
+    };
+    defer file.close();
+    var buf_rdr = std.io.bufferedReader(file.reader());
+    const rdr = buf_rdr.reader();
+
+    while (true) {
+        // clear buf each time
+        buf.clearRetainingCapacity();
+        // read each line and store it in buf
+        rdr.streamUntilDelimiter(buf.writer(), '\n', null) catch |err|
+            switch (err) {
+            // switch on err, if end of stream append and break
+            error.EndOfStream => {
+                if (buf.items.len > 0) {
+                    // append copy of buf to lines and trim
+                    if (buf.items[0] != '\n') {
+                        const trimmed_buf = std.mem.trim(u8, buf.items, " \r\n\t");
+                        try lines.append(try allocator.dupe(u8, trimmed_buf));
+                    }
+                }
+                break;
+            },
+            // err out
+            else => {
+                std.debug.print("Err: {}\n", .{err});
+                std.process.exit(1);
+            },
+        };
+        // append copy of buf to lines and trim
+        if (buf.items.len > 0) {
+            if (buf.items[0] != '\n') {
+                const trimmed_buf = std.mem.trim(u8, buf.items, " \r\n\t");
+                try lines.append(try allocator.dupe(u8, trimmed_buf));
+            }
+        }
+    }
 }
