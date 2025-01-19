e14c19b5eba61d18c1177c3abcf7cd41066871df
umer <bscs23211@itu.edu.pk>
2025-01-19 20:43:53 +0500

    chore: make bin for host

--- build.zig
+++ build.zig
@@ -1,28 +1,15 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = std.Target.Query{
-        .cpu_arch = .x86_64,
-        .os_tag = .linux,
-    };
-
     const exe = b.addExecutable(.{
-        .name = "sasm",
+        .name = "zhky",
         .root_source_file = b.path("main.zig"),
-        .target = b.resolveTargetQuery(target),
+        .target = b.host,
+        .optimize = .ReleaseFast,
+        .strip = true,
+        .single_threaded = true,
+        .unwind_tables = false,
     });
 
     b.installArtifact(exe);
-
-    // for zls
-    const exe_check = b.addExecutable(.{
-        .name = "sasm",
-        .root_source_file = b.path("main.zig"),
-        .target = b.resolveTargetQuery(target),
-    });
-
-    // check
-    // build but dont install bin
-    const check = b.step("check", "Check if foo compiles");
-    check.dependOn(&exe_check.step);
 }
