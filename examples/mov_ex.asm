a0f90e6ac25d56b5f3c0458208df0ac2b2df0f36
umer <bscs23211@itu.edu.pk>
2025-03-30 03:29:24 +0500

    feat: change example

--- examples/mov_ex.asm
+++ examples/mov_ex.asm
@@ -1,5 +1,11 @@
-mov$ ax% 9d%
-mov$ bx% ax%
-nop$
-mov$ cx% 4c00h%
+section .text
+    global _start
 
+_start:
+    mov eax, 0x9D    
+    mov ebx, eax    
+    nop             
+    mov ecx, 0x4C00     
+    mov eax, 1         
+    xor ebx, ebx      
+    int 0x80          
