b7289a9212b5889d4cea934cea97636e8bbbdf2a
umer <bscs23211@itu.edu.pk>
2025-03-30 03:28:58 +0500

    feat: add win64 output option

--- build.zig
+++ build.zig
@@ -1,15 +1,25 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
-    const exe = b.addExecutable(.{
-        .name = "zhky",
-        .root_source_file = b.path("main.zig"),
-        .target = b.host,
-        .optimize = .ReleaseFast,
-        .strip = true,
-        .single_threaded = true,
-        .unwind_tables = false,
-    });
+const tgs = [_]std.Target.Query{
+    .{
+        .cpu_arch = .x86_64,
+        .os_tag = .linux,
+    },
+    .{
+        .cpu_arch = .x86_64,
+        .os_tag = .windows,
+    },
+};
 
-    b.installArtifact(exe);
+pub fn build(b: *std.Build) void {
+    for (tgs) |tg| {
+        const exe = b.addExecutable(.{
+            .name = "zhky",
+            .root_source_file = b.path("main.zig"),
+            .target = b.resolveTargetQuery(tg),
+            .optimize = .Debug,
+        });
+
+        b.installArtifact(exe);
+    }
 }
