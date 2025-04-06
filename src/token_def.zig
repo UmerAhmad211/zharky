fe47589a89d368b73024cda50da87bcf1863f1a2
umer <bscs23211@itu.edu.pk>
2025-04-07 00:36:01 +0500

    refactor: classification of instructions based on types of operands

--- src/token_def.zig
+++ src/token_def.zig
@@ -2,7 +2,10 @@ pub const TokenType = enum {
     // multiple chars
     KEYWORD,
     SECTION_NAME,
-    INSTRUCTION,
+    INSTRUCTION_0OP,
+    INSTRUCTION_1OP,
+    INSTRUCTION_2OP,
+    INSTRUCTION_O1OP,
     SIZE,
     REG,
     IDENTIFIER,
@@ -36,18 +39,25 @@ pub const section_name = [_][]const u8{
     ".text", ".data",
 };
 
-pub const instructions = [_][]const u8{
-    "mov",  "add", "adc",  "sub",   "mul",
-    "div",  "pop", "push", "pusha", "xor",
-    "popa", "cmp", "dec",  "inc",   "hlt",
-    "nop",  "int", "neg",  "shl",   "shr",
-    "and",  "or",  "jmp",  "ja",    "jae",
-    "jb",   "jbe", "jg",   "jge",   "jl",
-    "jle",  "je",  "jne",  "jc",    "jnc",
-    "jo",   "jno", "js",   "jns",   "loop",
-    "call", "ret", "int",  "lea",   "xchng",
-    "sbb",  "not", "test", "iret",  "sar",
-    "ror",  "rol", "rcl",  "rcr",
+pub const instructions_0op = [_][]const u8{
+    "hlt", "nop", "pusha", "popa", "iret",
+};
+
+pub const instructions_1op = [_][]const u8{
+    "mul", "div",  "pop",  "push", "int", "neg", "shl", "shr",
+    "jmp", "ja",   "jae",  "jb",   "jbe", "jg",  "jge", "jl",
+    "jle", "je",   "jne",  "jc",   "jnc", "jo",  "jno", "js",
+    "jns", "loop", "call", "dec",  "inc", "not", "sar", "ror",
+    "rol", "rcl",  "rcr",
+};
+
+pub const instructions_2op = [_][]const u8{
+    "mov", "add", "adc", "sub", "cmp",  "xor",
+    "and", "or",  "lea", "sbb", "xchg", "test",
+};
+
+pub const instructions_optional_1op = [_][]const u8{
+    "ret",
 };
 
 pub const size = [_][]const u8{
