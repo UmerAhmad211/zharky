48c410aade6bcdf8dc8aaa305fa2e09276a14fe1
umer <bscs23211@itu.edu.pk>
2025-04-14 17:21:50 +0500

    chore: change decimal syntax in zharky

--- examples/hello.asm
+++ examples/hello.asm
@@ -6,12 +6,12 @@ section .text
 	global _start
 
 _start:
-	mov eax, 4          
-	mov ebx, 1         
-	mov ecx, msg        
-	mov edx, [len]      
-	int 80h            
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, msg
+	mov edx, [len]
+	int 80h
 
-	mov eax, 1d          
-	xor ebx, ebx       
-	int 80h          
+	mov eax, 1
+	xor ebx, ebx
+	int 80h
