83099f9a9167639bc8821b0e41b200bb1e2681f5
umer <bscs23211@itu.edu.pk>
2025-04-14 17:22:36 +0500

    chore: change decimal syntax in zharky

--- examples/mov_ex.asm
+++ examples/mov_ex.asm
@@ -1,11 +1,14 @@
+section .data
+    msg db 53
+
 section .text
     global _start
 
 _start:
-    mov eax, 0x9D    
+    mov eax, 9dh    
     mov ebx, eax    
     nop             
-    mov ecx, 0x4C00     
+    mov ecx, 4c00h     
     mov eax, 1         
     xor ebx, ebx      
-    int 0x80          
+    int 80h          
