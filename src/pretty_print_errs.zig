639e701d300937dad8273f14d21c209a66b5a1a9
umer <bscs23211@itu.edu.pk>
2025-04-27 01:09:03 +0500

    feat: add pretty printer for errors

---
+++ src/pretty_print_errs.zig
@@ -0,0 +1,73 @@
+const std = @import("std");
+const print = std.debug.print;
+
+const l = @import("lexer.zig");
+
+pub const compilerErrors = error{
+    invalidOperand,
+    missingOperand,
+    syntaxError,
+    unexpectedEOL,
+    unexpectedEOF,
+    eaxNoDisp,
+    wrongNumFormat,
+    onlyLabel,
+};
+
+pub const errorToken = struct {
+    err_token: l.Token,
+    error_type: compilerErrors,
+};
+
+pub const red = "\x1b[31m";
+pub const reset = "\x1b[0m";
+pub const blue = "\x1b[34m";
+pub const green = "\x1b[32m";
+
+pub fn printErrMsgAndExit(token: *errorToken) void {
+    switch (token.*.error_type) {
+        compilerErrors.invalidOperand => {
+            print("ZHARKY: " ++ red ++ "Error: " ++ reset ++ "Invalid operand:{d}:{d}.\n", .{ token.*.err_token.row_no, token.*.err_token.col_no });
+            printErrLine(token);
+        },
+        compilerErrors.missingOperand => {
+            print("ZHARKY: " ++ red ++ "Error: " ++ reset ++ "Missing operand:{d}:{d}.\n", .{ token.*.err_token.row_no, token.*.err_token.col_no });
+            printErrLine(token);
+        },
+        compilerErrors.syntaxError => {
+            print("ZHARKY: " ++ red ++ "Error: " ++ reset ++ "Syntax error:{d}:{d}.\n", .{ token.*.err_token.row_no, token.*.err_token.col_no });
+            printErrLine(token);
+        },
+        compilerErrors.unexpectedEOL => {
+            print("ZHARKY: " ++ red ++ "Error: " ++ reset ++ "Unexpected EOL:{d}:{d}.\n", .{ token.*.err_token.row_no, token.*.err_token.col_no });
+            printErrLine(token);
+        },
+        compilerErrors.unexpectedEOF => {
+            print("ZHARKY: " ++ red ++ "Error: " ++ reset ++ "Unexpected EOF:{d}:{d}.\n", .{ token.*.err_token.row_no, token.*.err_token.col_no });
+            printErrLine(token);
+        },
+        compilerErrors.eaxNoDisp => {
+            print("ZHARKY: " ++ red ++ "Error: " ++ reset ++ "EAX takes no displacement:{d}:{d}.\n", .{ token.*.err_token.row_no, token.*.err_token.col_no });
+            printErrLine(token);
+        },
+        compilerErrors.wrongNumFormat => {
+            print("ZHARKY: " ++ red ++ "Error: " ++ reset ++ "Wrong number format:{d}:{d}.\n", .{ token.*.err_token.row_no, token.*.err_token.col_no });
+            printErrLine(token);
+        },
+        compilerErrors.onlyLabel => {
+            print("ZHARKY: " ++ red ++ "Error: " ++ reset ++ "Only Label here:{d}:{d}.\n", .{ token.*.err_token.row_no, token.*.err_token.col_no });
+            printErrLine(token);
+        },
+    }
+    std.process.exit(1);
+}
+
+fn printErrLine(token: *errorToken) void {
+    print(blue ++ "{d}" ++ reset ++ " | {s}\n", .{ token.*.err_token.row_no, token.*.err_token.curr_line });
+    var i: usize = 0;
+    const limit: usize = token.*.err_token.col_no + std.math.log10(token.*.err_token.row_no) + 3;
+    while (i < limit) : (i += 1) {
+        print(" ", .{});
+    }
+    print(green ++ "^" ++ reset ++ "\n", .{});
+}
