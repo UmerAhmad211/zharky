be27883b4b5dd9d86e332889978a4cafec48e268
umer <bscs23211@itu.edu.pk>
2025-04-27 01:10:25 +0500

    feat: complete parser

--- src/parser.zig
+++ src/parser.zig
@@ -1,116 +1,378 @@
 const std = @import("std");
+const eql = std.mem.eql;
+
 const l = @import("lexer.zig");
+const pp = @import("pretty_print_errs.zig");
+const errorToken = pp.errorToken;
+const compilerError = pp.compilerErrors;
 const td = @import("token_def.zig");
 const TokenType = td.TokenType;
 const ut = @import("util.zig");
-const eql = std.mem.eql;
+const Number = ut.Number;
+
 var curr_token: l.Token = undefined;
-var curr_index: u32 = 0;
+var token_arr: ?*std.ArrayList(l.Token) = null;
+var curr_index: usize = 0;
 
-// TODO: throw error from custom error set
-pub fn parse(tokenized_input: *std.ArrayList(l.Token)) void {
-    expectDataSection(tokenized_input);
-    expectData(tokenized_input);
-    expectTextSection(tokenized_input);
-    expectText(tokenized_input);
+const operand = struct {
+    op_type: TokenType,
+    disp: ?Number,
+    value: ?Number,
+};
+
+const instruction = struct {
+    opcode: l.Token,
+    op1: ?operand,
+    op2: ?operand,
+};
+
+pub fn parse(tokenized_input: *std.ArrayList(l.Token), err_tok: *errorToken) bool {
+    token_arr = tokenized_input;
+    curr_token = token_arr.?.*.items[curr_index];
+
+    // errors bubbling
+    expectDataSection() catch |err| {
+        err_tok.*.error_type = err;
+        err_tok.*.err_token = curr_token;
+        return false;
+    };
+
+    expectData() catch |err| {
+        err_tok.*.error_type = err;
+        err_tok.*.err_token = curr_token;
+        return false;
+    };
+
+    expectTextSection() catch |err| {
+        err_tok.*.error_type = err;
+        err_tok.*.err_token = curr_token;
+        return false;
+    };
+
+    expectText() catch |err| {
+        err_tok.*.error_type = err;
+        err_tok.*.err_token = curr_token;
+        return false;
+    };
+    return true;
 }
 
-fn expectDataSection(tokenized_input: *std.ArrayList(l.Token)) void {
-    // zig fmt: off
-    if ((curr_index + 2 < tokenized_input.items.len)
-        and tokenized_input.items[curr_index].type == TokenType.K_SECTION
-        and tokenized_input.items[curr_index + 1].type == TokenType.D_SECTION
-        and tokenized_input.items[curr_index + 2].type == TokenType.EOL)
-    {
-        curr_index += 3;
-        curr_token = tokenized_input.items[curr_index];
-        return;
+// check data section key words i.e: section .data\n
+fn expectDataSection() compilerError!void {
+    const valid_token_arr = [3]TokenType{ .K_SECTION, .D_SECTION, .EOL };
+    inline for (valid_token_arr) |tok| {
+        if (curr_token.type == tok) {
+            nextToken();
+        } else {
+            return compilerError.syntaxError;
+        }
     }
-    // zig fmt: on
-    ut.printErrMsgAndExit("Error at data section.");
 }
 
-fn expectTextSection(tokenized_input: *std.ArrayList(l.Token)) void {
-    // zig fmt: off
-    if ((curr_index + 5 < tokenized_input.items.len)
-        and tokenized_input.items[curr_index].type     == TokenType.K_SECTION
-        and tokenized_input.items[curr_index + 1].type == TokenType.T_SECTION
-        and tokenized_input.items[curr_index + 2].type == TokenType.EOL
-        and tokenized_input.items[curr_index + 3].type == TokenType.K_GLOBAL
-        and tokenized_input.items[curr_index + 4].type == TokenType.IDENTIFIER
-        and tokenized_input.items[curr_index + 5].type == TokenType.EOL)
-    {
-        curr_index += 6;
-        curr_token = tokenized_input.items[curr_index];
-        return;
+// section .text\n
+// global _start\n
+fn expectTextSection() compilerError!void {
+    const valid_token_arr = [6]TokenType{ .K_SECTION, .T_SECTION, .EOL, .K_GLOBAL, .IDENTIFIER, .EOL };
+    inline for (valid_token_arr) |tok| {
+        if (curr_token.type == tok) {
+            nextToken();
+        } else {
+            return compilerError.syntaxError;
+        }
     }
-    // zig fmt: on
-    ut.printErrMsgAndExit("Error at text section.");
 }
 
-fn expectData(tokenized_input: *std.ArrayList(l.Token)) void {
-    while (curr_token.type == TokenType.IDENTIFIER) {
-        if (curr_token.type == TokenType.IDENTIFIER) nextToken(tokenized_input) else {
-            ut.printErrMsgAndExit("Expect ID in data section.");
+// msg db "Hi", Ah\n => ok
+// msg db "hi",\n    => error
+fn expectData() compilerError!void {
+    while (curr_token.type == .IDENTIFIER) {
+        if (curr_token.type == .IDENTIFIER) nextToken() else {
+            return compilerError.syntaxError;
         }
         // zig fmt: off
-        if (curr_token.type == TokenType.DB 
-            or curr_token.type == TokenType.DW
-            or curr_token.type == TokenType.DD) nextToken(tokenized_input) else {
-            ut.printErrMsgAndExit("Expect data size in data section");
+        if (curr_token.type == .DB 
+            or curr_token.type == .DD) nextToken() else {
+            return compilerError.syntaxError;
         }
-        if (curr_token.type == TokenType.STRING 
-            or curr_token.type == TokenType.CHAR 
-            or curr_token.type == TokenType.IMM
-            or curr_token.type == TokenType.NUM) nextToken(tokenized_input) else {
-            ut.printErrMsgAndExit("Expect data in data section.");
-        }
-        const seeked_token = seekToken(tokenized_input);
-        if (curr_token.type == TokenType.COMMA
-            and (seeked_token == TokenType.STRING
-            or seeked_token == TokenType.CHAR
-            or seeked_token == TokenType.IMM
-            or seeked_token == TokenType.NUM)) nextToken(tokenized_input)
         // zig fmt: on
-        else if (curr_token.type == TokenType.EOL) {
-            nextToken(tokenized_input);
-            return;
-        } else {
-            ut.printErrMsgAndExit("Expect data type, standalone comma not allowd.");
+        if (!checkForDataCommaPattern()) {
+            return compilerError.syntaxError;
         }
-        nextToken(tokenized_input);
-        if (curr_token.type == TokenType.EOL)
-            nextToken(tokenized_input)
+        nextToken();
+    }
+}
+
+// check operands and syntax
+fn expectText() compilerError!void {
+    // zig fmt: off
+    while (curr_token.type != .EOF) {
+        var inst_line: instruction = undefined;
+        if (curr_token.type == .IDENTIFIER) {
+            nextToken();
+            if (curr_token.type == .COLON) {
+                nextToken();
+            } else {
+                return compilerError.syntaxError;
+            }
+        }
+        else if (curr_token.type == .INSTRUCTION_0OP) {
+            inst_line.opcode = curr_token;
+            nextToken();
+        }
+        else if (curr_token.type == .INSTRUCTION_1OP) {
+            inst_line = checkOperands() catch |err| return err;
+        }
+        else if (curr_token.type == .INSTRUCTION_2OP) {
+            inst_line = checkOperands() catch |err| return err;
+        }
+        else if (curr_token.type == .INSTRUCTION_O1OP) {
+            inst_line = checkOperands() catch |err| return err;
+        }
         else {
-            ut.printErrMsgAndExit("Expect one data directive in one line.");
+            return compilerError.syntaxError;
         }
+
+        if (curr_token.type != .EOL) {
+            return compilerError.syntaxError;
+        } 
+        nextToken();
     }
+    // zig fmt: on
 }
 
-// to be changed
-fn expectText(tokenzied_input: *std.ArrayList(l.Token)) void {
-    while (curr_token.type != TokenType.EOF) {
-        if (curr_token.type == TokenType.IDENTIFIER) {
-            nextToken(tokenzied_input);
-            if (curr_token.type == TokenType.COLON) nextToken(tokenzied_input) else ut.printErrMsgAndExit("Syntax error, colon expected.");
-            if (curr_token.type == TokenType.EOL) nextToken(tokenzied_input) else ut.printErrMsgAndExit("Sytax error, one label reference at one line.");
-        } else if (curr_token.type == TokenType.INSTRUCTION_0OP)
-            nextToken(tokenzied_input)
-        else if (curr_token.type == TokenType.INSTRUCTION_1OP) {
-            nextToken(tokenzied_input);
-            if (curr_token.type == TokenType.IDENTIFIER or curr_token.type == TokenType.REG) nextToken(tokenzied_input) else ut.printErrMsgAndExit("Syntax error,Wrong operand");
-        }
-    }
-}
-
-inline fn nextToken(tokenized_input: *std.ArrayList(l.Token)) void {
-    if (curr_token.type != TokenType.EOF) {
+// if EOF has occurred return .ERR
+fn nextToken() void {
+    if (curr_token.type != .EOF) {
         curr_index += 1;
-        curr_token = tokenized_input.items[curr_index];
+        curr_token = token_arr.?.items[curr_index];
+    } else {
+        curr_token = .{ .type = .ERR, .value = undefined, .curr_line = undefined, .row_no = undefined, .col_no = undefined };
     }
 }
 
-fn seekToken(tokenized_input: *std.ArrayList(l.Token)) TokenType {
-    if (curr_token.type != TokenType.EOF) return tokenized_input.items[curr_index + 1].type;
-    return TokenType.ERR;
+// .ERR if .EOF
+fn seekToken() TokenType {
+    if (curr_token.type != .EOF) return token_arr.?.items[curr_index + 1].type;
+    return .ERR;
+}
+
+// .STRING .COMMA data .EOL => ok
+// data .COMMA .EOL         => bad
+fn checkForDataCommaPattern() bool {
+    // zig fmt: off
+    while (curr_token.type != .EOL) {
+        if (curr_token.type == .STRING
+            or curr_token.type == .CHAR
+            or curr_token.type == .IMM
+            or curr_token.type == .NUM) nextToken() else {
+            return false;
+        }
+        if(curr_token.type == .EOL) {
+            continue;
+        }
+        if(curr_token.type == .COMMA){
+            const seeked_token = seekToken();
+            if(seeked_token == .EOL){
+                return false;
+            }
+            nextToken();
+        }
+        else {
+            return false;
+        }
+    }
+    // zig fmt: on
+    return true;
+}
+
+fn createMem() compilerError!operand {
+    var n_op: operand = undefined;
+    n_op.op_type = .MEM;
+    var pm: u2 = 2;
+
+    if (curr_token.type == .IDENTIFIER) {
+        n_op.value = Number{ .slice = curr_token.value };
+    } else if (curr_token.type == .IMM) {
+        n_op.disp = ut.isANumOfAnyBase(curr_token.value, .IMM) catch return compilerError.wrongNumFormat;
+    } else if (curr_token.type == .REG) {
+        if (eql(u8, curr_token.value, "eax")) {
+            const seekd_token = seekToken();
+            if (seekd_token != .C_BRACKET) return compilerError.eaxNoDisp;
+        }
+        n_op.value = Number{ .slice = curr_token.value };
+    } else {
+        return compilerError.syntaxError;
+    }
+    nextToken();
+
+    if (curr_token.type == .C_BRACKET) {
+        nextToken();
+        if (curr_token.type == .EOL) {
+            nextToken();
+            return n_op;
+        }
+        return compilerError.syntaxError;
+    } else if (curr_token.type == .PLUS) {} else if (curr_token.type == .MINUS) {
+        pm = 1;
+    } else {
+        return compilerError.syntaxError;
+    }
+
+    nextToken();
+
+    if (curr_token.type == .IMM) {
+        if (pm == 0) {
+            n_op.disp = ut.isANumOfAnyBase(curr_token.value, .PLUS) catch return compilerError.wrongNumFormat;
+        } else if (pm == 1) {
+            n_op.disp = ut.isANumOfAnyBase(curr_token.value, .MINUS) catch return compilerError.wrongNumFormat;
+        } else {
+            n_op.disp = ut.isANumOfAnyBase(curr_token.value, .IMM) catch return compilerError.wrongNumFormat;
+        }
+    } else {
+        return compilerError.syntaxError;
+    }
+    return n_op;
+}
+
+fn checkOperands() compilerError!instruction {
+    var n_op: operand = undefined;
+    var n_inst: instruction = undefined;
+    var seekd_token = seekToken();
+    if (curr_token.type == .INSTRUCTION_1OP) {
+        // zig fmt: off
+        if(seekd_token == .REG or seekd_token == .O_BRACKET){
+            if(eql(u8, curr_token.value, "mul")
+            or eql(u8, curr_token.value, "div")
+            or eql(u8, curr_token.value, "pop")
+            or eql(u8, curr_token.value, "push")
+            or eql(u8, curr_token.value, "neg")
+            or eql(u8, curr_token.value, "jmp")
+            or eql(u8, curr_token.value, "call")
+            or eql(u8, curr_token.value, "dec")
+            or eql(u8, curr_token.value, "inc")
+            or eql(u8, curr_token.value, "not")){
+                n_inst.opcode = curr_token;
+                nextToken();
+                if(seekd_token == .O_BRACKET){
+                    n_op = createMem() catch |err| return err;
+                }
+                else {
+                    n_op.value = Number{.slice = curr_token.value};
+                    n_op.op_type = .REG;
+                }
+                n_inst.op1 = n_op;
+            }
+        }
+        else if(seekd_token == .IMM){
+            if(eql(u8, curr_token.value, "int")
+            or eql(u8, curr_token.value, "push")){
+                n_inst.opcode = curr_token;
+                nextToken();
+                const convd_num =  ut.isANumOfAnyBase(curr_token.value,.IMM) catch return compilerError.syntaxError;
+                n_op.value = convd_num; 
+                n_op.op_type = curr_token.type;
+                n_inst.op1 = n_op;
+            }
+        }
+        else if(seekd_token == .IDENTIFIER){
+            if(eql(u8, curr_token.value, "jmp")
+            or eql(u8, curr_token.value, "je")
+            or eql(u8, curr_token.value, "jne")
+            or eql(u8, curr_token.value, "call")){
+                n_inst.opcode = curr_token;
+                nextToken();
+                // should be in symb table
+                n_op.value = Number{.slice = curr_token.value};
+                n_op.op_type = .IDENTIFIER;
+                n_inst.op1 = n_op;
+            }
+        }
+        else {return compilerError.syntaxError;}
+    }
+    else if(curr_token.type == .INSTRUCTION_2OP){
+            var n_op2: operand = undefined;
+            if(eql(u8, curr_token.value, "mov")){
+                if(seekd_token == .REG or seekd_token == .O_BRACKET){
+                    n_inst.opcode = curr_token;
+                    nextToken();
+                    if(seekd_token == .O_BRACKET){
+                        n_op = createMem() catch |err| return err;
+                    }
+                    else {
+                        n_op.value = Number{.slice = curr_token.value};
+                        n_op.op_type = .REG;
+                    }
+                    n_inst.op1 = n_op;
+                }
+            nextToken();
+            if(curr_token.type == .COMMA){}
+            else {return compilerError.syntaxError;}
+            seekd_token = seekToken();
+            if(seekd_token == .REG or seekd_token == .O_BRACKET or seekd_token == .IMM or seekd_token == .IDENTIFIER){
+                nextToken();
+                if(seekd_token == .O_BRACKET){
+                    if(n_inst.op1.?.op_type == .MEM) {return compilerError.invalidOperand;}
+                    else {n_op2 = createMem() catch |err| return err;}
+                }
+                else {
+                    n_op2.op_type = curr_token.type;
+                    if(curr_token.type == .REG or curr_token.type == .IDENTIFIER)
+                        n_op2.value = Number{.slice = curr_token.value} else {
+                        n_op2.value = ut.isANumOfAnyBase(curr_token.value,.IMM) catch return compilerError.wrongNumFormat;
+                    }
+                }
+                n_inst.op2 = n_op2;
+            }
+            else {return compilerError.syntaxError;}
+        }
+        else if(eql(u8, curr_token.value, "add")
+             or eql(u8, curr_token.value, "adc")
+             or eql(u8, curr_token.value, "sub")
+             or eql(u8, curr_token.value, "cmp")
+             or eql(u8, curr_token.value, "xor")
+             or eql(u8, curr_token.value, "and")
+             or eql(u8, curr_token.value, "or")
+             or eql(u8, curr_token.value, "test")){
+            if(seekd_token == .REG or seekd_token == .O_BRACKET){
+                n_inst.opcode = curr_token;
+                nextToken();
+                if(seekd_token == .O_BRACKET){
+                    n_op = createMem() catch |err| return err;
+                }
+                else {
+                    n_op.value = Number{.slice = curr_token.value};
+                    n_op.op_type = .REG;
+                }
+            }
+            nextToken();
+            if(curr_token.type == .COMMA){}
+            else {return compilerError.syntaxError;}
+            seekd_token = seekToken();
+            if(seekd_token == .REG or seekd_token == .IMM){
+                nextToken();
+                n_op2.op_type = curr_token.type;
+                if(curr_token.type == .REG)
+                    n_op2.value = Number{.slice = curr_token.value} else {
+                    n_op2.value = ut.isANumOfAnyBase(curr_token.value,.IMM) catch return compilerError.wrongNumFormat; 
+                }
+            }
+            else {return compilerError.syntaxError;}
+        }
+    }
+    else if(curr_token.type == .INSTRUCTION_O1OP){
+        n_inst.opcode = curr_token;
+        if(seekd_token == .IMM){
+            nextToken();
+            n_op.op_type = seekd_token;
+            const convd_num = ut.isANumOfAnyBase(curr_token.value,.IMM) catch return compilerError.wrongNumFormat;
+            n_op.value = convd_num;
+            n_inst.op1 = n_op;
+        }
+        else if(seekd_token == .EOL){nextToken();}
+        else {return compilerError.syntaxError;}    
+    }
+    else {return compilerError.syntaxError;}
+    nextToken();
+    if(curr_token.type != .EOL) return compilerError.syntaxError;
+    return n_inst;
 }
