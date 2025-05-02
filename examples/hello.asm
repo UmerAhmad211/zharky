60d3a2d251540dc62b362f5c5e6613c01d2f8d83
umer <bscs23211@itu.edu.pk>
2025-05-03 01:01:28 +0500

    example: show update subroutine example

--- examples/hello.asm
+++ examples/hello.asm
@@ -1,17 +1,24 @@
 section .data
-	msg db "Hello, world!", Ah  
-	len dd 14                    
+    hello_msg db "hello, world!", ah
+   	len dd 14 
 
 section .text
-	global _start
+    global _start 
+
+print_hello:
+    mov eax, 4          
+    mov ebx, 1         
+    mov ecx, hello_msg
+    mov edx, [len + 2] 
+    int 80h
+    ret
+
+exit_program:
+    mov eax, 1          
+    xor ebx, ebx        
+    int 80h
+    ret
 
 _start:
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, msg
-	mov edx, [len]
-	int 80h
-
-	mov eax, 1
-	xor ebx, ebx
-	int 80h
+    call print_hello    
+    call exit_program  
