deb4091fe32282b1613f52f8b82ca4e69c0f4d8c
umer <bscs23211@itu.edu.pk>
2025-05-12 17:01:36 +0500

    build: change to release mode

--- build.zig
+++ build.zig
@@ -1,22 +1,23 @@
 const std = @import("std");
 
+const tg: []const std.Target.Query = &.{
+    .{ .cpu_arch = .x86_64, .os_tag = .linux },
+    .{ .cpu_arch = .x86_64, .os_tag = .windows },
+};
+
 pub fn build(b: *std.Build) void {
-    const module = b.addModule("zhky", .{
-        .root_source_file = b.path("main.zig"),
-        .target = b.graph.host,
-        .optimize = .Debug,
-    });
-    const exe = b.addExecutable(.{
-        .name = "zhky",
-        .root_module = module,
-    });
-    b.installArtifact(exe);
-
-    const exe_check = b.addExecutable(.{
-        .name = "zhky",
-        .root_module = module,
-    });
-
-    const check = b.step("check", "Check if zhkarky compiles");
-    check.dependOn(&exe_check.step);
+    for (tg) |t| {
+        const module = b.addModule("zhky", .{
+            .root_source_file = b.path("main.zig"),
+            .target = b.resolveTargetQuery(t),
+            .strip = true,
+            .single_threaded = true,
+            .optimize = .ReleaseFast,
+        });
+        const exe = b.addExecutable(.{
+            .name = "zhky",
+            .root_module = module,
+        });
+        b.installArtifact(exe);
+    }
 }
