6b0b0b711799e79dca4c12f2a0d4fe8929ceed6b
UmerAhmad211 <umerahmed824@gmail.com>
2025-06-27 20:18:03 +0500

    Use smp_allocator for linux, c_allocator for windows, error handling for codegen, chmod final binary

--- src/asm_driver.zig
+++ src/asm_driver.zig
@@ -1,7 +1,9 @@
 const std = @import("std");
+const bltn = @import("builtin");
 
 const cg = @import("codegen.zig");
 const l = @import("lexer.zig");
+const Lexer = l.Lexer;
 const p = @import("parser.zig");
 const instruction = p.instruction;
 const operand = p.operand;
@@ -19,16 +21,12 @@ const Number = ut.Number;
 const Line = ut.Line;
 
 pub fn process(file_name: []const u8) compilerError!void {
-    // allocator
-    var gpa = std.heap.DebugAllocator(.{}){};
-    defer _ = gpa.deinit();
-    var allocator = gpa.allocator();
+    var allocator: std.mem.Allocator = undefined;
+    if (bltn.target.os.tag == .windows) allocator = std.heap.c_allocator else allocator = std.heap.smp_allocator;
 
-    // allocate lines
     var lines = std.MultiArrayList(Line){};
     defer lines.deinit(allocator);
 
-    // free lines
     defer for (lines.items(.ln)) |line|
         allocator.free(line);
 
@@ -39,36 +37,28 @@ pub fn process(file_name: []const u8) compilerError!void {
         pp.printErrMsgAndExit(&err_tok);
     };
 
-    // lexer tokens
+    // lex
     var tokenized_input = std.ArrayList(l.Token).init(allocator);
     defer tokenized_input.deinit();
 
     var s_table: symbolTable = .init(&allocator);
     defer s_table.deinit();
 
-    // lexer loop
     for (lines.items(.ln), lines.items(.ln_no)) |lne, lne_n| {
-        if (!l.tokenizeInputStream(lne, &tokenized_input, lne_n, &err_tok)) pp.printErrMsgAndExit(&err_tok);
+        if (!Lexer.tokenizeInputStream(lne, &tokenized_input, lne_n, &err_tok)) pp.printErrMsgAndExit(&err_tok);
     }
 
     const tok_eof: l.Token = .{ .type = TokenType.EOF, .value = "eof", .curr_line = undefined, .row_no = lines.items(.ln_no)[lines.len - 1], .col_no = 1 };
     tokenized_input.append(tok_eof) catch {
-        err_tok.err_token = tok_eof;
         err_tok.error_type = compilerError.programError;
         pp.printErrMsgAndExit(&err_tok);
     };
 
-    // hold text
     var text_arr = std.ArrayList(instruction).init(allocator);
     defer text_arr.deinit();
 
-    // hold data
     var data_arr = std.ArrayList(u8).init(allocator);
     defer data_arr.deinit();
-
-    // stores header meta data
-    var header = std.ArrayList(u8).init(allocator);
-    defer header.deinit();
 
     var data_len: u32 = 0;
     var text_len: u32 = 0;
@@ -81,18 +71,12 @@ pub fn process(file_name: []const u8) compilerError!void {
     var encodings = std.ArrayList(u8).init(allocator);
     defer encodings.deinit();
 
-    // set target headers
-    if (std.mem.eql(u8, ut.out_file_type, "-elf32")) {
-        tg_elf.tgELF32(&header) catch return compilerError.programError;
-    } else if (std.mem.eql(u8, ut.out_file_type, "-pe32")) {
-        tg_win.tgWIN32(&header);
-    }
+    var header = comptime tg_elf.tgELF32();
 
-    // calculate pad
-    const pad_1 = [_]u8{0} ** 0x0F8C;
+    const pad_1 = comptime [_]u8{0} ** 0x0F8C;
     const pad_2_len: u32 = @abs(0x1000 - text_len);
 
-    cg.codegenDriver(&encodings, &text_arr, &s_table, &header, 0x1000 + text_len + pad_2_len) catch |err| return err;
+    if (!cg.codegenDriver(&encodings, &text_arr, &s_table, &header, 0x1000 + text_len + pad_2_len, &err_tok)) pp.printErrMsgAndExit(&err_tok);
 
     const old_len = encodings.items.len;
     encodings.resize(old_len + pad_2_len) catch return compilerError.programError;
@@ -100,12 +84,16 @@ pub fn process(file_name: []const u8) compilerError!void {
 
     tg_elf.updateELF(&header, text_len, data_len, 0x1000 + text_len + pad_2_len);
 
-    // file
     const file = std.fs.cwd().createFile(ut.out_file_name, .{ .truncate = true }) catch return compilerError.writeCreateFileErr;
     defer file.close();
 
-    _ = file.writeAll(header.items) catch return compilerError.writeCreateFileErr;
-    _ = file.writeAll(&pad_1) catch return compilerError.writeCreateFileErr;
-    _ = file.writeAll(encodings.items) catch return compilerError.writeCreateFileErr;
-    _ = file.writeAll(data_arr.items) catch return compilerError.writeCreateFileErr;
+    file.writeAll(&header) catch return compilerError.writeCreateFileErr;
+    file.writeAll(&pad_1) catch return compilerError.writeCreateFileErr;
+    file.writeAll(encodings.items) catch return compilerError.writeCreateFileErr;
+    file.writeAll(data_arr.items) catch return compilerError.writeCreateFileErr;
+
+    const c_out_file_name = std.mem.Allocator.dupeZ(allocator, u8, ut.out_file_name) catch return compilerError.programError;
+    defer allocator.free(c_out_file_name);
+    const ptr_out_file_name = c_out_file_name.ptr;
+    _ = std.os.linux.chmod(ptr_out_file_name, 0o755);
 }
